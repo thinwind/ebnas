41fc6f8bb935eb74dc4200fd1845efa56a1b68f3
Shang <niceshang@icloud.com>
2021-11-12 10:58:25 +0800

    修改启动脚本

--- docker/start-nacos-8848.sh
+++ docker/start-nacos-8848.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+docker container stop ebnas-8848
 docker container rm ebnas-8848
-LOCAL_IP=`ifconfig | grep 192.168 | awk '{print $2}'`
-docker run --name ebnas-8848 -p 8848:8848 -e HOST_IP=$LOCAL_IP -d ebnas:a4 -m standalone
+# LOCAL_IP=`ifconfig | grep 192.168 | awk '{print $2}'`
+docker run --name ebnas-8848 -p 8848:8848 -d ebnas:a4 -m standalone
--- docker/start-nacos-8849.sh
+++ docker/start-nacos-8849.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+docker container stop ebnas-8849
 docker container rm ebnas-8849
-LOCAL_IP=`ifconfig | grep 192.168 | awk '{print $2}'`
-docker run --name ebnas-8849 -p 8849:8848 -e HOST_IP=$LOCAL_IP -d ebnas:a4 -m standalone
+# LOCAL_IP=`ifconfig | grep 192.168 | awk '{print $2}'`
+docker run --name ebnas-8849 -p 8849:8848 -d ebnas:a4 -m standalone
