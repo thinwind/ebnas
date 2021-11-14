ee3d847380989d3d9ecf5e26743c2bc3d2261a49
Shang <niceshang@icloud.com>
2021-11-14 12:11:42 +0800

    修改8849

--- docker/start-nacos-8849.sh
+++ docker/start-nacos-8849.sh
@@ -2,5 +2,6 @@
 
 docker container stop ebnas-8849
 docker container rm ebnas-8849
-# LOCAL_IP=`ifconfig | grep 192.168 | awk '{print $2}'`
-docker run --name ebnas-8849 -p 8849:8848 -d ebnas:a4 -m standalone
+
+LOCAL_IP=`ifconfig | grep 192.168 | awk '{print $2}'`
+docker run -e JAVA_OPT="-Debnas.cluster-name=cluster2 -Debnas.clusterhouse=${LOCAL_IP}:7031 -Dserver.port=8849" -e HOST_IP=${LOCAL_IP} --name ebnas-8849 -p 8849:8849 -d ebnas:hb3 -m standalone 
